88ee75e24d397f4d7b82db63d16742d415d13564
nirmala <nirmalakuppusamy89@gmail.com>
2021-09-25 21:54:50 +0100

    Moved files to appropriate locations

--- src/test/java/com/gic/invoice_manager/InvoiceRunner.java
+++ src/test/java/com/gic/invoice_manager/InvoiceRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/com/gic/invoice_manager/features",
+        features = "src/test/resources/features",
         glue = {""},
         tags = "@invoice-manager",
         dryRun = false)
