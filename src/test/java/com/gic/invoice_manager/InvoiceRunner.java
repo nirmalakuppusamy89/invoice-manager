134bdb649e2e8be30ea6394a562908f0739f5e5f
nirmala <nirmalakuppusamy89@gmail.com>
2021-09-28 16:43:24 +0100

    Changing to default runner tag

--- src/test/java/com/gic/invoice_manager/InvoiceRunner.java
+++ src/test/java/com/gic/invoice_manager/InvoiceRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {""},
-        tags = "@create-invoice",
+        tags = "@invoice-manager",
         dryRun = false)
 public class InvoiceRunner {
 }
