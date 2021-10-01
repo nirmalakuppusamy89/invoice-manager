0b34a1ffc86302678e6b9a801dd8fa18e9232d90
nirmala <nirmalakuppusamy89@gmail.com>
2021-10-02 00:39:11 +0100

    Added test scenarios to export diff file formats and add customer

--- src/test/java/com/gic/invoice_manager/steps/ExportInvoiceSteps.java
+++ src/test/java/com/gic/invoice_manager/steps/ExportInvoiceSteps.java
@@ -1,6 +1,7 @@
 package com.gic.invoice_manager.steps;
 
 import com.gic.invoice_manager.pageObjects.AddInvoicePage;
+import com.gic.invoice_manager.pageObjects.ExportInvoicePage;
 import com.gic.invoice_manager.pageObjects.ListInvoicePage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
@@ -10,26 +11,28 @@ public class ExportInvoiceSteps {
 
     private final ListInvoicePage listInvoicePage;
     private final AddInvoicePage addInvoicePage;
+    private final ExportInvoicePage exportInvoicePage;
 
 
     public ExportInvoiceSteps() {
         this.listInvoicePage = new ListInvoicePage();
         this.addInvoicePage = new AddInvoicePage();
+        this.exportInvoicePage = new ExportInvoicePage();
 
     }
 
 
     @When("I navigates to list invoices page")
     public void iNavigatesToListInvoicesPage() {
-    addInvoicePage.redirectToHomePage();
-    listInvoicePage.goToListInvoicePage();
-    listInvoicePage.verifyUserInListInvoicePage();
+        addInvoicePage.redirectToHomePage();
+        listInvoicePage.goToListInvoicePage();
+        listInvoicePage.verifyUserInListInvoicePage();
 
     }
 
     @And("I can download the table content in different formats")
     public void iCanDownloadTheTableContentInDifferentFormats() {
-
+        exportInvoicePage.exportInvoiceDifferentFormat();
 
     }
 
